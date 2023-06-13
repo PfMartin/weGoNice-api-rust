a6bcea643e4afda505b13fb9be6b3c2d3dd9b3b6
Martin <martinpfatrisch@gmail.com>
2023-06-13 17:28:27 +0200

    Created UsersClient

--- src/api/user_api.rs
+++ src/api/user_api.rs
@@ -1,9 +1,9 @@
-use crate::{models::user_model::User, repository::mongo_repo::MongoRepo};
+use crate::{database::users_client::UsersClient, models::user_model::User};
 use rocket::{get, http::Status, serde::json::Json, State};
 
 #[get("/users")]
-pub fn get_all_users(db: &State<MongoRepo>) -> Result<Json<Vec<User>>, Status> {
-    let users = db.get_all_users();
+pub async fn get_all_users(db: &State<UsersClient>) -> Result<Json<Vec<User>>, Status> {
+    let users = db.get_all_users().await;
     match users {
         Ok(users) => Ok(Json(users)),
         Err(_) => Err(Status::InternalServerError),
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 mod api;
+mod database;
 mod models;
-mod repository;
+mod utils;
 
 use api::user_api::get_all_users;
 
-use repository::mongo_repo::MongoRepo;
+use database::users_client::UsersClient;
 use rocket::{launch, routes};
 
 #[launch]
-fn rocket() -> _ {
-    let db = MongoRepo::init();
+async fn rocket() -> _ {
+    let db = match UsersClient::new().await {
+        Ok(d) => d,
+        Err(err) => panic!("Couldn't connect to User db: {}", err),
+    };
 
     rocket::build()
         .manage(db)
